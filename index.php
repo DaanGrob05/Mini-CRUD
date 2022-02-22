54399fd5001cc78b4487926611f6e083ce4ca717
DaanGrob05 <d.grob05@gmail.com>
2022-02-22 12:59:24 +0100

    beetje stijl toegevoegd en php bestand aangemaakt

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link
+			href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+			rel="stylesheet"
+			integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+			crossorigin="anonymous"
+		/>
+		<link
+			rel="stylesheet"
+			href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"
+		/>
+		<title>'t Smikkelhoekje</title>
+	</head>
+
+	<body class="d-flex flex-column h-100 w-100 min-vh-100 bg-dark">
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="">'t Smikkelhoekje</a>
+					<button
+						class="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div
+						class="collapse navbar-collapse"
+						id="navbarSupportedContent"
+					>
+						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+							<li class="nav-item">
+								<a
+									class="nav-link active"
+									aria-current="page"
+									href=""
+									>Home</a
+								>
+							</li>
+						</ul>
+						<form class="d-flex">
+							<input
+								class="form-control me-2"
+								type="search"
+								placeholder="Dit werkt nog niet"
+								aria-label="Search"
+							/>
+							<button class="btn btn-outline-success" type="submit">
+								Zoek
+							</button>
+						</form>
+					</div>
+				</div>
+			</nav>
+		</header>
+
+		<div class="container bg-dark pb-3 h-100 text-light">
+			<main>
+				jo
+			</main>
+		</div>
+
+		<footer class="border-top footer mt-auto pt-3 text-muted bg-dark">
+			&copy; yeet
+    	</footer>
+
+		<!-- Bootstrap -->
+		<script
+			src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+			integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+			crossorigin="anonymous"
+		></script>
+	</body>
+</html>
