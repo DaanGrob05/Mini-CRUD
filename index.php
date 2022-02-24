1f34385227cb64a3f6519b56e2ef593a21730939
DaanGrob05 <d.grob05@gmail.com>
2022-02-24 09:48:44 +0100

    menu pagina en beautify

--- index.php
+++ index.php
@@ -1,95 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<!-- Bootstrap -->
-		<link
-			href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-			rel="stylesheet"
-			integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-			crossorigin="anonymous"
-		/>
-		<link
-			rel="stylesheet"
-			href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"
-		/>
-		<!-- Eigen CSS -->
-		<link rel="stylesheet" href="css/main.css">
-		<title>'t Smikkelhoekje</title>
-	</head>
 
-	<body class="d-flex flex-column min-vh-100">
-		<!-- Header -->
-		<header>
-			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-				<div class="container-fluid">
-					<a class="navbar-brand" href="">'t Smikkelhoekje</a>
-					<button
-						class="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarSupportedContent"
-						aria-controls="navbarSupportedContent"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					<div
-						class="collapse navbar-collapse"
-						id="navbarSupportedContent"
-					>
-						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-							<li class="nav-item">
-								<a
-									class="nav-link active"
-									aria-current="page"
-									href=""
-									>Menu
-								</a>
-							</li>
-							<li class="nav-item">
-								<a href="" class="nav-link">Contact</a>
-							</li>
-							<li class="nav-item">
-								<a href="" class="nav-link">Login</a>
-							</li>
-						</ul>
-						<form class="d-flex">
-							<input
-								class="form-control me-2"
-								type="search"
-								placeholder="Dit werkt nog niet"
-								aria-label="Search"
-							/>
-							<button class="btn btn-outline-success" type="submit">
-								Zoek
-							</button>
-						</form>
-					</div>
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!-- Bootstrap -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" />
+	<!-- Eigen CSS -->
+	<link rel="stylesheet" href="css/main.css">
+	<title>'t Smikkelhoekje</title>
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+	<!-- Header -->
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="index.php">'t Smikkelhoekje</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+					aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+							<a href="menu.php" class="nav-link">Menu</a>
+						</li>
+						<li class="nav-item">
+							<a href="" class="nav-link">Contact</a>
+						</li>
+						<li class="nav-item">
+							<a href="" class="nav-link">Login</a>
+						</li>
+					</ul>
+					<form class="d-flex">
+						<input class="form-control me-2" type="search" placeholder="Dit werkt nog niet"
+							aria-label="Search" />
+						<button class="btn btn-outline-success" type="submit">
+							Zoek
+						</button>
+					</form>
 				</div>
-			</nav>
-		</header>
-		<!-- End Header -->
+			</div>
+		</nav>
+	</header>
+	<!-- End Header -->
 
-		<!-- Main Content -->
-		<main class="container">
-			<p>Hoi</p>
-		</main>
-		<!-- End Main Content -->
+	<!-- Main Content -->
+	<main class="container">
+		<p>Hoi</p>
+	</main>
+	<!-- End Main Content -->
 
-		<!-- Footer -->
-		<footer class="footer mt-auto p-3 text-muted bg-dark">
-				<p>'t Smikkelhoekje </p>
-    	</footer>
+	<!-- Footer -->
+	<footer class="footer mt-auto p-3 text-muted bg-dark">
+		<p>'t Smikkelhoekje </p>
+	</footer>
 
-		<!-- Bootstrap -->
-		<script
-			src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-			integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-			crossorigin="anonymous"
-		></script>
-	</body>
+	<!-- Bootstrap -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+	</script>
+</body>
+
 </html>
