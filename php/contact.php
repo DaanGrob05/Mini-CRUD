a052ce9073ad2ea75db2d441c07e1141e987e7d9
DaanGrob05 <d.grob05@gmail.com>
2022-02-27 11:15:31 +0100

    contact begin

--- php/contact.php
+++ php/contact.php
@@ -10,8 +10,40 @@
     <?php include "../html/header.html" ?>
 
     <!-- Main Content -->
-    <main>
+    <main class="container">
+        <!--Section: Contact v.2-->
+        <section class="mb-4">
 
+            <!--Section heading-->
+            <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+            <!--Section description-->
+            <p class="text-center w-responsive mx-auto mb-5">Als u vragen heeft kunt u gerust contact met ons opnemen met de onderstaande gegevens.</p>
+
+            <div class="row">
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="text-center">
+                    <ul class="list-unstyled mb-0">
+                        <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                            <p>Cafetaria 't Smikkelhoekje Nijmegen 6525BD</p>
+                        </li>
+
+                        <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                            <p>0620691433</p>
+                        </li>
+
+                        <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                            <p>d.grob05@gmail.com</p>
+                        </li>
+                    </ul>
+                </div>
+                <!--Grid column-->
+
+            </div>
+
+        </section>
+        <!--Section: Contact v.2-->
     </main>
 
     <!-- Footer en Bootstrap Scripts -->
