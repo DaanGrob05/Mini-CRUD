4eedd1d092e9bc539f8f5ec3ca7d08e93caad9ea
DaanGrob05 <d.grob05@gmail.com>
2022-03-15 11:30:51 +0100

    update

--- php/index.php
+++ php/index.php
@@ -8,7 +8,7 @@
     ?>
 
     <!-- Main Content -->
-    <main class="bg-image">
+    <main>
         <!-- <p>hoi</p> -->
     </main>
 
--- php/login.php
+++ php/login.php
@@ -8,18 +8,20 @@
     ?>
 
     <!-- Main Content -->
-    <main class="container pt-3 w-50">
-        <form action="login.php" method="post">
-            <div class="mb-3">
-                <label class="form-label">Gebruikersnaam</label>
-                <input type="text" class="form-control" name="username">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Wachtwoord</label>
-                <input type="password" class="form-control" name="password">
-            </div>
-            <button type="submit" class="btn btn-primary">Log in</button>
-        </form>
+    <main>
+        <div class="container pt-3 w-50">
+            <form action="login.php" method="post">
+                <div class="mb-3">
+                    <label class="form-label">Gebruikersnaam</label>
+                    <input type="text" class="form-control" name="username">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Wachtwoord</label>
+                    <input type="password" class="form-control" name="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Log in</button>
+            </form>
+        </div>
     </main>
 
     <!-- Footer en Bootstrap Scripts -->
