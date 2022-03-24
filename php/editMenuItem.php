3bb4ec3770d5da0ce9b4fc67fc78871700904e98
DaanGrob05 <d.grob05@gmail.com>
2022-03-24 09:21:43 +0100

    update

--- php/editMenuItem.php
+++ php/editMenuItem.php
@@ -10,31 +10,43 @@
 <div class="container">
     <?php
         $res = $result[0];
-        echo $res['artiest'] . "<br>";
-        echo $res['genre'] . "<br>";
+        // echo $res['artiest'] . "<br>";
+        // echo $res['genre'] . "<br>";
 
         // TODO Aanpas functionaliteit
         // Pas aan
-        echo "<div class='container pt-3'>";
-            echo "<form method='post' action='redirects/redirect.php?page=menuItem'>";
-                echo "<div class='form-group'>";
-                    echo "<input type='hidden' class='form-control' name='menuItemID' value='" . $_POST['menuItemID'] . "'>";
-                echo "</div>";
+        echo "<form method='post' action='redirects/redirect.php?page=menuItem' class='pt-3'>";
+            // Item ID
+            echo "<div class='form-group'>";
+                echo "<input type='hidden' class='form-control' name='menuItemID' value='" . $_POST['menuItemID'] . "'>";
+            echo "</div>";
 
-                echo "<div class='form-group'>";
-                    echo "<label class='form-label'>Naam</label>";
-                    echo "<input type='text' name='itemName' value='" . $res['titel'] . "'></input>";
-                echo "</div>";
+            // Titel
+            echo "<div class='form-group'>";
+                echo "<label class='form-label'>Naam</label>";
+                echo "<input type='text' name='titel' value='" . $res['titel'] . "'></input>";
+            echo "</div>";
+            
+            // Artiest
+            echo "<div class='form-group'>";
+                echo "<label class='form-label'>Artiest</label>";
+                echo "<input type='text' name='artiest' value='" . $res['artiest'] . "'></input>";
+            echo "</div>";
 
-                echo "<button type='submit' class='btn btn-primary btn-sm' name='editMenuItem' value='true' >Pas Aan</button>";
-            echo "</form>";
-        echo "</div>";
+            // Genre
+            echo "<div class='form-group'>";
+                echo "<label class='form-label'>Genre</label>";
+                echo "<input type='text' name='genre' value='" . $res['genre'] . "'></input>";
+            echo "</div>";
+
+            echo "<button type='submit' class='btn btn-primary' name='editMenuItem' value='true' >Pas Aan</button>";
+        echo "</form>";
 
 
         // Verwijder
-        echo "<form method='post' class='form-control-sm' action='redirects/redirect.php?page=menuItem'>";
+        echo "<form method='post' action='redirects/redirect.php?page=menuItem'>";
             echo "<input type='hidden' name='menuItemID' value='" . $_POST['menuItemID'] . "'>";
-            echo "<button type='submit' class='btn btn-danger btn-sm' name='deleteMenuItem' value='true'>Verwijder</button>";
+            echo "<button type='submit' class='btn btn-danger' name='deleteMenuItem' value='true'>Verwijder</button>";
         echo "</form>";
     ?>
 </div>
