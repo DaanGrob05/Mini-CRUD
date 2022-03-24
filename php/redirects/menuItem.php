8fc19086302735639bb049846a5706204a459481
DaanGrob05 <86193398+DaanGrob05@users.noreply.github.com>
2022-03-24 09:23:16 +0100

    update (#1)

--- php/redirects/menuItem.php
+++ php/redirects/menuItem.php
@@ -1,4 +1,20 @@
 <?php
+    if (isset($_POST['editMenuItem'])) {
+        $sql = "UPDATE `album` SET `titel`='[value-2]',`artiest`='[value-3]',`genre`='[value-4]' WHERE ID = :ID";
+        $stmt = $connect->prepare($sql);
+        $stmt->bindParam(':titel', $_POST['titel']);
+        $stmt->bindParam(':artiest', $_POST['artiest']);
+        $stmt->bindParam(':genre', $_POST['genre']);
+        $stmt->bindParam(":ID", $_POST['menuItemID']);
+        $stmt->execute();
+
+        session_start();
+        $_SESSION['menuItemEdited'] = true;
+    }
+
+
+
+
     if (isset($_POST['deleteMenuItem'])) {
         $sql = "DELETE FROM `album` WHERE ID = :ID";
         $stmt = $connect->prepare($sql);
